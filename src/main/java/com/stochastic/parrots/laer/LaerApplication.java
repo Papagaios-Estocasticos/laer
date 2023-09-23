8d01a054c695c6927afdd7713188972642627a47
Guilherme Rocha <67191432+guiPython@users.noreply.github.com>
2023-09-23 12:06:38 -0300

    add: health route to check service

--- src/main/java/com/stochastic/parrots/laer/LaerApplication.java
+++ src/main/java/com/stochastic/parrots/laer/LaerApplication.java
@@ -2,11 +2,20 @@ package com.stochastic.parrots.laer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
 
 @SpringBootApplication
+@RestController
 public class LaerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LaerApplication.class, args);
 	}
+
+	@GetMapping("health")
+	@ResponseStatus(HttpStatus.OK)
+	public void health() {}
 }
