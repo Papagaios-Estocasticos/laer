55ce3f5508a4d97416c559bcdf2229fac19fb1c4
guiPython <guipython2001@gmail.com>
2023-09-25 19:22:57 -0300

    test: fix email test names

--- src/test/java/com/stochastic/parrots/laer/ut/domain/identity/EmailTest.java
+++ src/test/java/com/stochastic/parrots/laer/ut/domain/identity/EmailTest.java
@@ -11,23 +11,23 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class EmailTest {
 
     @Test
-    public void createName() {
+    public void createEmail() {
         var email = Email.of("test@test.com");
         assertEquals("test@test.com", email.get());
     }
 
     @Test
-    public void throwsInvalidNameWhenDoNotRegexMatch() {
+    public void throwsInvalidEmailWhenDoNotRegexMatch() {
         assertThrows(Email.Invalid.class, () -> Email.of("test.com"));
     }
 
     @Test
-    public void throwsInvalidNameWhenCreateWithEmptyString() {
+    public void throwsInvalidEmailWhenCreateWithEmptyString() {
         assertThrows(Email.Invalid.class, () -> Email.of(""));
     }
 
     @Test
-    public void throwsInvalidNameWhenCreateWithBlankString() {
+    public void throwsInvalidEmailWhenCreateWithBlankString() {
         assertThrows(Email.Invalid.class, () -> Email.of("     "));
         assertThrows(Email.Invalid.class, () -> Email.of(" "));
     }
