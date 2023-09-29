a290ff09efa6454d2a185856f467d8c37bf214bc
guiPython <guipython2001@gmail.com>
2023-09-29 13:44:24 -0300

    add: interest value object

---
+++ src/test/java/com/stochastic/parrots/laer/ut/domain/assets/commons/InterestTest.java
@@ -0,0 +1,83 @@
+package com.stochastic.parrots.laer.ut.domain.assets.commons;
+
+import com.stochastic.parrots.laer.domain.assets.commons.Interest;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class InterestTest {
+
+    @Test
+    public void createInterestByPercentage() {
+        var interest = Interest.percentage(10.0);
+
+        assertEquals("interest", interest.errors().context());
+        assertEquals(0.1, interest.rate());
+        assertFalse(interest.hasErrors());
+    }
+
+    @Test
+    public void createInterestByRate() {
+        var interest = Interest.rate(0.1);
+
+        assertEquals("interest", interest.errors().context());
+        assertEquals(0.1, interest.rate());
+        assertFalse(interest.hasErrors());
+    }
+
+    @Test
+    public void createInterestWithErrorWhenPassNegativeValue() {
+        var interest = Interest.percentage(-1);
+
+        assertEquals("interest", interest.errors().context());
+        assertTrue(interest.hasErrors());
+        assertThat(interest.errors().reasons())
+                .hasSize(1)
+                .contains("interest rate must be between 0.0 and 1.0");
+    }
+
+    @Test
+    public void createInterestWithErrorWhenPassValueGreaterOne() {
+        var interest = Interest.rate(1.5);
+
+        assertEquals("interest", interest.errors().context());
+        assertTrue(interest.hasErrors());
+        assertThat(interest.errors().reasons())
+                .hasSize(1)
+                .contains("interest rate must be between 0.0 and 1.0");
+    }
+
+    @Test
+    public void createInterestWithErrorWhenPassValueGreaterOneHundred() {
+        var interest = Interest.percentage(105);
+
+        assertEquals("interest", interest.errors().context());
+        assertTrue(interest.hasErrors());
+        assertThat(interest.errors().reasons())
+                .hasSize(1)
+                .contains("interest rate must be between 0.0 and 1.0");
+    }
+
+    @Test
+    public void hashCodes() {
+        var interest = Interest.percentage(100.0).hashCode();
+        var sameInterest = Interest.rate(1.0).hashCode();
+        var other = Interest.percentage(50.0).hashCode();
+
+        assertEquals(interest, sameInterest);
+        assertNotEquals(interest, other);
+    }
+
+    @Test
+    public void equals() {
+        var interest = Interest.percentage(100.0);
+        var sameInterest = Interest.rate(1.0);
+        var other = Interest.percentage(50.0);
+
+        assertEquals(interest, sameInterest);
+        assertNotEquals(interest, other);
+    }
+}
