4fcfce2597765d270e0b4571c97467704cf22266
guiPython <guipython2001@gmail.com>
2023-09-29 13:45:55 -0300

    add: bond information (aggregate all commons value objects)

---
+++ src/main/java/com/stochastic/parrots/laer/domain/assets/bonds/Information.java
@@ -0,0 +1,85 @@
+package com.stochastic.parrots.laer.domain.assets.bonds;
+
+import com.stochastic.parrots.laer.domain.assets.commons.*;
+import com.stochastic.parrots.laer.domain.shared.ValueObject;
+
+import java.time.LocalDate;
+import java.util.function.Supplier;
+
+public class Information extends ValueObject {
+    private static final String CONTEXT = "bond information";
+
+    public final String name;
+    public final Issuer issuer;
+    public final Price price;
+    public final LocalDate maturity;
+    public final boolean isIncomeTaxExempt;
+
+    protected Information(Builder builder) {
+        super(CONTEXT);
+        this.name = builder.name;
+        this.issuer = builder.issuer.get();
+        this.price = Price.of(builder.price);
+        this.maturity = builder.maturity;
+        this.isIncomeTaxExempt = builder.isIncomeTaxExempt;
+        validate();
+    }
+
+    private void validate() {
+        validate(issuer, price);
+        if(name.isBlank() || name.isEmpty()) {
+            errors.because("bond name cannot be empty");
+            return;
+        }
+        if(name.length() < 3 || name.length() > 17) {
+            errors.because("bond name must contain 3 to 17 letters");
+        }
+    }
+
+    public static class Builder {
+        private String name;
+        private Supplier<Issuer> issuer;
+        private Double price;
+        private LocalDate maturity;
+        private boolean isIncomeTaxExempt = false;
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder issuer(Supplier<Issuer> issuer) {
+            this.issuer = issuer;
+            return this;
+        }
+
+        public Builder issuer(String issuer) {
+            this.issuer = () -> Issuer.bank(issuer);
+            return this;
+        }
+
+        public Builder price(Double price) {
+            this.price = price;
+            return this;
+        }
+
+        public Builder maturity(LocalDate maturity) {
+            this.maturity = maturity;
+            return this;
+        }
+
+        public Builder incomeTaxExemption() {
+            this.isIncomeTaxExempt = true;
+            return this;
+        }
+
+        public Builder notIncomeTaxExemption() {
+            this.isIncomeTaxExempt = false;
+            return this;
+        }
+
+        public Information build() {
+            return new Information(this);
+        }
+    }
+}
---
+++ src/test/java/com/stochastic/parrots/laer/ut/domain/assets/bonds/InformationTest.java
@@ -0,0 +1,102 @@
+package com.stochastic.parrots.laer.ut.domain.assets.bonds;
+
+import com.stochastic.parrots.laer.domain.assets.commons.Issuer;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static com.stochastic.parrots.laer.fixture.domain.assets.commons.InformationFixture.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class InformationTest {
+
+    @Test
+    public void createInformationWithIncomeTaxException() {
+        var now = LocalDate.now();
+        var information = someInformationWithIncomeTaxException()
+                .name("some bond")
+                .issuer(() -> Issuer.bank("some bank"))
+                .maturity(now)
+                .price(100.00).build();
+
+
+        assertEquals("some bond", information.name);
+        assertEquals("some bank", information.issuer.get());
+        assertEquals(100.00, information.price.get());
+        assertEquals(now, information.maturity);
+        assertTrue(information.isIncomeTaxExempt);
+        assertFalse(information.hasErrors());
+    }
+
+    @Test
+    public void createInformationWithoutIncomeTaxException() {
+        var now = LocalDate.now();
+        var information = someInformationWithoutIncomeTaxException()
+                .name("some bond")
+                .issuer(() -> Issuer.bank("some bank"))
+                .maturity(now)
+                .price(100.00).build();
+
+
+        assertEquals("some bond", information.name);
+        assertEquals("some bank", information.issuer.get());
+        assertEquals(100.00, information.price.get());
+        assertEquals(now, information.maturity);
+        assertFalse(information.isIncomeTaxExempt);
+        assertFalse(information.hasErrors());
+    }
+
+    @Test
+    public void createInformationWithErrorWhenPassEmptyName() {
+        var information = someInformation().name("").build();
+
+        assertEquals("bond information", information.errors().context());
+        assertTrue(information.hasErrors());
+        assertThat(information.errors().reasons())
+                .hasSize(1)
+                .contains("bond name cannot be empty");
+    }
+
+
+    @Test
+    public void createInformationWithErrorWhenPassInvalidIssuer() {
+        var information = someInformationWithoutIssuer()
+                .issuer("test").build();
+
+        assertEquals("bond information", information.errors().context());
+        assertTrue(information.hasErrors());
+        assertThat(information.errors().reasons())
+                .hasSize(1)
+                .contains("issuer name must contain 5 to 10 letters");
+    }
+
+    @Test
+    public void createInformationWithErrorWhenPassInvalidPrice() {
+        var information = someInformation()
+                .price(-1.0).build();
+
+        assertEquals("bond information", information.errors().context());
+        assertTrue(information.hasErrors());
+        assertThat(information.errors().reasons())
+                .hasSize(1)
+                .contains("price must be greater 0.0");
+    }
+
+    @Test
+    public void createInformationWithErrorWhenPassInvalidPriceIssuerAndName() {
+        var information = someInvalidInformation().build();
+
+        assertEquals("bond information", information.errors().context());
+        assertTrue(information.hasErrors());
+        assertThat(information.errors().reasons())
+                .hasSize(3)
+                .containsAll(List.of(
+                        "price must be greater 0.0",
+                        "issuer name must contain 5 to 10 letters",
+                        "bond name must contain 3 to 17 letters"));
+    }
+}
