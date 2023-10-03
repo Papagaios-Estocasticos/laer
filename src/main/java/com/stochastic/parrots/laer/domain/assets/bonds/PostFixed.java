b7cd43df828b94244df78951d7ad1c3013db662e
guiPython <guipython2001@gmail.com>
2023-10-02 21:26:41 -0300

    fix: remove factory methods

--- src/main/java/com/stochastic/parrots/laer/domain/assets/bonds/PostFixed.java
+++ src/main/java/com/stochastic/parrots/laer/domain/assets/bonds/PostFixed.java
@@ -1,6 +1,5 @@
 package com.stochastic.parrots.laer.domain.assets.bonds;
 
-import com.stochastic.parrots.laer.domain.assets.commons.Issuer;
 import com.stochastic.parrots.laer.domain.assets.indexers.Indexer;
 
 public class PostFixed extends Bond {
@@ -8,53 +7,30 @@ public class PostFixed extends Bond {
     private final Indexer indexer;
 
     private PostFixed(Builder builder) {
-        super(builder.id, builder.information, CONTEXT);
+        super(builder.id, builder.information, builder.isIncomeTaxExempt, CONTEXT);
         this.indexer = builder.indexer;
         validate(information);
     }
 
-    private static PostFixed withoutIncomeTax(Long id, Information.Builder information, Indexer indexer) {
+    public static PostFixed withoutIncomeTax(Long id, Information information, Indexer indexer) {
         return new Builder()
                 .id(id)
-                .with(information.incomeTaxExemption())
+                .with(information)
+                .incomeTaxExemption()
                 .indexer(indexer).build();
     }
 
-    public static PostFixed lca(Long id, Information.Builder information, Indexer indexer) {
-        return withoutIncomeTax(id, information.name("LCA"), indexer);
-    }
 
-    public static PostFixed lci(Long id, Information.Builder information, Indexer indexer) {
-        return withoutIncomeTax(id, information.name("LCI"), indexer);
-    }
-
-    public static PostFixed cra(Long id, Information.Builder information, Indexer indexer) {
-        return withoutIncomeTax(id, information.name("CRA"), indexer);
-    }
-
-    public static PostFixed cri(Long id, Information.Builder information, Indexer indexer) {
-        return withoutIncomeTax(id, information.name("CRI"), indexer);
-    }
-
-    public static PostFixed cdb(Long id, Information.Builder information, Indexer indexer) {
+    public static PostFixed withIncomeTax(Long id, Information information, Indexer indexer) {
         return new Builder()
                 .id(id)
-                .with(information.name("CDB").notIncomeTaxExemption())
-                .indexer(indexer).build();
-    }
-
-    public static PostFixed treasury(Long id, Information.Builder information, Indexer indexer) {
-        return new Builder()
-                .id(id)
-                .with(information.name("Tesouro").notIncomeTaxExemption().issuer(Issuer::government))
+                .with(information)
+                .notIncomeTaxExemption()
                 .indexer(indexer).build();
     }
 
     public String name() {
-        if(issuer().equals(Issuer.government())) {
-            return String.format("%s %s %d", information.name, indexer, maturity().getYear());
-        }
-        return String.format("%s %s %d %s", information.name, information.issuer, maturity().getYear(), indexer);
+        return String.format("%s %d %s", information.name, maturity().getYear(), indexer);
     }
 
     public Indexer indexer() {
@@ -65,14 +41,25 @@ public class PostFixed extends Bond {
         private Long id;
         private Information information;
         private Indexer indexer;
+        private boolean isIncomeTaxExempt;
 
         public Builder id(Long id) {
             this.id = id;
             return this;
         }
 
-        public Builder with(Information.Builder builder) {
-            this.information = builder.build();
+        public Builder with(Information information) {
+            this.information = information;
+            return this;
+        }
+
+        private Builder incomeTaxExemption() {
+            this.isIncomeTaxExempt = true;
+            return this;
+        }
+
+        private Builder notIncomeTaxExemption() {
+            this.isIncomeTaxExempt = false;
             return this;
         }
 
--- src/test/java/com/stochastic/parrots/laer/ut/domain/assets/bonds/PostFixedTest.java
+++ src/test/java/com/stochastic/parrots/laer/ut/domain/assets/bonds/PostFixedTest.java
@@ -1,9 +1,7 @@
 package com.stochastic.parrots.laer.ut.domain.assets.bonds;
 
-import com.stochastic.parrots.laer.domain.assets.bonds.Information;
+import com.stochastic.parrots.laer.domain.assets.bonds.Bond;
 import com.stochastic.parrots.laer.domain.assets.bonds.PostFixed;
-import com.stochastic.parrots.laer.domain.assets.bonds.PreFixed;
-import com.stochastic.parrots.laer.domain.assets.commons.Issuer;
 import com.stochastic.parrots.laer.domain.assets.indexers.IPCA;
 import com.stochastic.parrots.laer.domain.assets.indexers.Indexer;
 import com.stochastic.parrots.laer.domain.shared.Entity;
@@ -12,10 +10,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static com.stochastic.parrots.laer.domain.assets.commons.Interest.percentage;
-import static com.stochastic.parrots.laer.fixture.domain.assets.commons.InformationFixture.someInformation;
-import static com.stochastic.parrots.laer.fixture.domain.assets.commons.InformationFixture.someInvalidInformation;
+import static com.stochastic.parrots.laer.fixture.domain.assets.commons.InformationFixture.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -24,132 +23,64 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class PostFixedTest {
 
     @Test
-    public void createTreasury() {
-        var treasury = PostFixed.treasury(123L, someInformation().maturity(MATURITY), INDEXER);
-
-        assertEquals(123L, treasury.id());
-        assertEquals(String.format("Tesouro %s %d", INDEXER, MATURITY.getYear()), treasury.name());
-        assertEquals(INFORMATION.price, treasury.price());
-        assertEquals(Issuer.government(), treasury.issuer());
-        assertEquals(INFORMATION.maturity, treasury.maturity());
-        assertEquals(INDEXER, treasury.indexer());
-        assertEquals(0.225, treasury.incomeTaxRate());
-        assertTrue(treasury.isActive());
-    }
-
-    @Test
-    public void createCdb() {
-        var cdb = PostFixed.cdb(123L,  someInformation().maturity(MATURITY), INDEXER);
+    public void createWithIncomeTax() {
+        var information = someCDBInformationWith(MATURITY);
+        var cdb = PostFixed.withIncomeTax(123L, information, INDEXER);
 
         assertEquals(123L, cdb.id());
-        assertEquals(String.format("CDB %s %d %s", INFORMATION.issuer, MATURITY.getYear(), INDEXER), cdb.name());
-        assertEquals(INFORMATION.price, cdb.price());
-        assertEquals(INFORMATION.issuer, cdb.issuer());
-        assertEquals(INFORMATION.maturity, cdb.maturity());
+        assertEquals(String.format("CDB %d %s", information.maturity.getYear(), cdb.indexer()), cdb.name());
+        assertEquals(information.price, cdb.price());
+        assertEquals(information.issuer, cdb.issuer());
+        assertEquals(information.maturity, cdb.maturity());
         assertEquals(INDEXER, cdb.indexer());
         assertEquals(0.225, cdb.incomeTaxRate());
         assertTrue(cdb.isActive());
     }
 
     @Test
-    public void incomeTaxRate() {
-        var incomeTaxRates = List.of(
-                PostFixed.lca(123L, someInformation(), INDEXER)
-                        .incomeTaxRate(),
-                PostFixed.cdb(123L, someInformation().maturity(NOW.plusDays(721)), INDEXER)
-                        .incomeTaxRate(),
-                PostFixed.cdb(123L, someInformation().maturity(NOW.plusDays(361)), INDEXER)
-                        .incomeTaxRate(),
-                PostFixed.cdb(123L, someInformation().maturity(NOW.plusDays(181)), INDEXER)
-                        .incomeTaxRate(),
-                PostFixed.cdb(123L, someInformation().maturity(NOW), INDEXER).incomeTaxRate());
-
-        assertEquals(List.of(0.0, 0.15, 0.175, 0.20, 0.225), incomeTaxRates);
-    }
-
-    @Test
-    public void createLca() {
-        var lca = PostFixed.lca(123L,  someInformation().maturity(MATURITY), INDEXER);
+    public void createWithoutIncomeTax() {
+        var information = someLCAInformationWith(MATURITY);
+        var lca = PostFixed.withoutIncomeTax(123L, information, INDEXER);
 
         assertEquals(123L, lca.id());
-        assertEquals(String.format("LCA %s %d %s", INFORMATION.issuer, MATURITY.getYear(), lca.indexer()), lca.name());
-        assertEquals(INFORMATION.price, lca.price());
-        assertEquals(INFORMATION.issuer, lca.issuer());
-        assertEquals(INFORMATION.maturity, lca.maturity());
+        assertEquals(String.format("LCA %d %s", information.maturity.getYear(), lca.indexer()), lca.name());
+        assertEquals(information.price, lca.price());
+        assertEquals(information.issuer, lca.issuer());
+        assertEquals(information.maturity, lca.maturity());
         assertEquals(INDEXER, lca.indexer());
         assertEquals(0.0, lca.incomeTaxRate());
         assertTrue(lca.isActive());
     }
 
     @Test
-    public void createLci() {
-        var lci = PostFixed.lci(123L,  someInformation().maturity(MATURITY), INDEXER);
+    public void incomeTaxRate() {
+        var incomeTaxRates = Stream.of(
+                PostFixed.withoutIncomeTax(1L, someLCAInformationWith(NOW), INDEXER),
+                PostFixed.withIncomeTax(2L, someCDBInformationWith(NOW.plusDays(721)), INDEXER),
+                PostFixed.withIncomeTax(3L, someCDBInformationWith(NOW.plusDays(361)), INDEXER),
+                PostFixed.withIncomeTax(4L, someCDBInformationWith(NOW.plusDays(181)), INDEXER),
+                PostFixed.withIncomeTax(5L, someCDBInformationWith(NOW), INDEXER))
+                        .map(Bond::incomeTaxRate).collect(Collectors.toList());
 
-        assertEquals(123L, lci.id());
-        assertEquals(String.format("LCI %s %d %s", INFORMATION.issuer, MATURITY.getYear(), lci.indexer()), lci.name());
-        assertEquals(INFORMATION.price, lci.price());
-        assertEquals(INFORMATION.issuer, lci.issuer());
-        assertEquals(INFORMATION.maturity, lci.maturity());
-        assertEquals(INDEXER, lci.indexer());
-        assertEquals(0.0, lci.incomeTaxRate());
-        assertTrue(lci.isActive());
-    }
-
-    @Test
-    public void createCra() {
-        var cra = PostFixed.cra(123L, someInformation().maturity(MATURITY), INDEXER);
-
-        assertEquals(123L, cra.id());
-        assertEquals(String.format("CRA %s %d %s", INFORMATION.issuer, MATURITY.getYear(), cra.indexer()), cra.name());
-        assertEquals(INFORMATION.price, cra.price());
-        assertEquals(INFORMATION.issuer, cra.issuer());
-        assertEquals(INFORMATION.maturity, cra.maturity());
-        assertEquals(INDEXER, cra.indexer());
-        assertEquals(0.0, cra.incomeTaxRate());
-        assertTrue(cra.isActive());
-    }
-
-    @Test
-    public void createCri() {
-        var cri = PostFixed.cri(123L, someInformation().maturity(MATURITY), INDEXER);
-
-        assertEquals(123L, cri.id());
-        assertEquals(String.format("CRI %s %d %s", INFORMATION.issuer, MATURITY.getYear(), cri.indexer()), cri.name());
-        assertEquals(INFORMATION.price, cri.price());
-        assertEquals(INFORMATION.issuer, cri.issuer());
-        assertEquals(INFORMATION.maturity, cri.maturity());
-        assertEquals(INDEXER, cri.indexer());
-        assertEquals(0.0, cri.incomeTaxRate());
-        assertTrue(cri.isActive());
-    }
-
-    @Test
-    public void throwsInvalidEntityExceptionWhenPassInvalidInformation() {
-        var exception = assertThrows(Entity.InvalidEntity.class, () ->
-                PreFixed.treasury(123L, someInformation().price(-1.0), percentage(5)));
-
-        assertEquals("pre-fixed bond", exception.errors().context());
-        assertThat(exception.errors().reasons())
-                .hasSize(1)
-                .containsAll(List.of("price must be greater 0.0"));
+        assertEquals(List.of(0.0, 0.15, 0.175, 0.20, 0.225), incomeTaxRates);
     }
 
     @Test
     public void throwsInvalidEntityExceptionWhenPassInvalidValueObjects() {
         var exception = assertThrows(Entity.InvalidEntity.class, () ->
-                PreFixed.cdb(123L, someInvalidInformation(), percentage(-5)));
+                PostFixed.withIncomeTax(123L, someInformationWithAllFieldsInvalid(), INDEXER));
 
-        assertEquals("pre-fixed bond", exception.errors().context());
+        assertEquals("post-fixed bond", exception.errors().context());
         assertThat(exception.errors().reasons())
-                .hasSize(3)
+                .hasSize(4)
                 .containsAll(List.of(
+                        "bond name must be in ['CDB', 'TESOURO']",
+                        "bond name must contain 3 to 17 letters",
                         "price must be greater 0.0",
-                        "issuer name must contain 5 to 10 letters",
-                        "interest rate must be greater 0.0"));
+                        "issuer name must contain 5 to 10 letters"));
     }
 
     private static final LocalDate NOW = LocalDate.now();
     private static final LocalDate MATURITY = NOW.plusDays(10);
     private static final Indexer INDEXER = IPCA.plus(percentage(5.0));
-    private static final Information INFORMATION = someInformation().maturity(MATURITY).build();
 }
