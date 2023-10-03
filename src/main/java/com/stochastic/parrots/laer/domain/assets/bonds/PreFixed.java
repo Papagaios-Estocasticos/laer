46986c3d17b48982383ed90d5ec30bf9328c8ed0
guiPython <guipython2001@gmail.com>
2023-10-02 21:26:33 -0300

    fix: remove factory methods

--- src/main/java/com/stochastic/parrots/laer/domain/assets/bonds/PreFixed.java
+++ src/main/java/com/stochastic/parrots/laer/domain/assets/bonds/PreFixed.java
@@ -1,59 +1,30 @@
 package com.stochastic.parrots.laer.domain.assets.bonds;
 
 import com.stochastic.parrots.laer.domain.assets.commons.Interest;
-import com.stochastic.parrots.laer.domain.assets.commons.Issuer;
 
 public class PreFixed extends Bond {
     private static final String CONTEXT = "pre-fixed bond";
     private final Interest interest;
 
     private PreFixed(Builder builder) {
-        super(builder.id, builder.information, CONTEXT);
+        super(builder.id, builder.information, builder.isIncomeTaxExempt, CONTEXT);
         this.interest = builder.interest;
         validate();
     }
 
-    private static PreFixed withoutIncomeTax(Long id, Information.Builder information, Interest interest) {
+    public static PreFixed withoutIncomeTax(Long id, Information information, Interest interest) {
         return new Builder()
                 .id(id)
-                .with(information.incomeTaxExemption().build())
+                .with(information)
+                .incomeTaxExemption()
                 .interest(interest).build();
     }
 
-    public static PreFixed lca(Long id, Information.Builder information, Interest interest) {
-        return withoutIncomeTax(id, information.name("LCA"), interest);
-    }
-
-    public static PreFixed lci(Long id, Information.Builder information, Interest interest) {
-        return withoutIncomeTax(id, information.name("LCI"), interest);
-    }
-
-    public static PreFixed cra(Long id, Information.Builder information, Interest interest) {
-        return withoutIncomeTax(id, information.name("CRA"), interest);
-    }
-
-    public static PreFixed cri(Long id, Information.Builder information, Interest interest) {
-        return withoutIncomeTax(id, information.name("CRI"), interest);
-    }
-
-    public static PreFixed cdb(Long id, Information.Builder information, Interest interest) {
+    public static PreFixed withIncomeTax(Long id, Information information, Interest interest) {
         return new Builder()
                 .id(id)
-                .with(information
-                        .name("CDB")
-                        .notIncomeTaxExemption()
-                        .build())
-                .interest(interest).build();
-    }
-
-    public static PreFixed treasury(Long id, Information.Builder information, Interest interest) {
-        return new Builder()
-                .id(id)
-                .with(information
-                        .name("Tesouro Préfixado")
-                        .notIncomeTaxExemption()
-                        .issuer(Issuer::government)
-                        .build())
+                .with(information)
+                .notIncomeTaxExemption()
                 .interest(interest).build();
     }
 
@@ -62,10 +33,7 @@ public class PreFixed extends Bond {
     }
 
     public String name() {
-        if (issuer().equals(Issuer.government())) {
-            return String.format("%s %d %s", information.name, maturity().getYear(), interest());
-        }
-        return String.format("%s %s %d %s", information.name, information.issuer, maturity().getYear(), interest());
+        return String.format("%s %d %s", information.name, maturity().getYear(), interest());
     }
 
     public Interest interest() {
@@ -76,23 +44,34 @@ public class PreFixed extends Bond {
         private Long id;
         private Information information;
         private Interest interest;
+        private boolean isIncomeTaxExempt;
 
-        public Builder id(Long id) {
+        private Builder id(Long id) {
             this.id = id;
             return this;
         }
 
-        public Builder with(Information information) {
+        private Builder with(Information information) {
             this.information = information;
             return this;
         }
 
-        public Builder interest(Interest interest) {
+        private Builder interest(Interest interest) {
             this.interest = interest;
             return this;
         }
 
-        public PreFixed build() {
+        private Builder incomeTaxExemption() {
+            this.isIncomeTaxExempt = true;
+            return this;
+        }
+
+        private Builder notIncomeTaxExemption() {
+            this.isIncomeTaxExempt = false;
+            return this;
+        }
+
+        private PreFixed build() {
             return new PreFixed(this);
         }
     }
--- src/test/java/com/stochastic/parrots/laer/ut/domain/assets/bonds/PreFixedTest.java
+++ src/test/java/com/stochastic/parrots/laer/ut/domain/assets/bonds/PreFixedTest.java
@@ -1,18 +1,18 @@
 package com.stochastic.parrots.laer.ut.domain.assets.bonds;
 
-import com.stochastic.parrots.laer.domain.assets.bonds.Information;
+import com.stochastic.parrots.laer.domain.assets.bonds.Bond;
 import com.stochastic.parrots.laer.domain.assets.bonds.PreFixed;
-import com.stochastic.parrots.laer.domain.assets.commons.Issuer;
 import com.stochastic.parrots.laer.domain.shared.Entity;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static com.stochastic.parrots.laer.domain.assets.commons.Interest.percentage;
-import static com.stochastic.parrots.laer.fixture.domain.assets.commons.InformationFixture.someInformation;
-import static com.stochastic.parrots.laer.fixture.domain.assets.commons.InformationFixture.someInvalidInformation;
+import static com.stochastic.parrots.laer.fixture.domain.assets.commons.InformationFixture.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,125 +20,61 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PreFixedTest {
 
     @Test
-    public void createTreasury() {
-        var treasury = PreFixed.treasury(123L, someInformation().maturity(MATURITY), percentage(13.0));
+    public void createWithIncomeTax() {
+        var information = someCDBInformationWith(MATURITY);
+        var cdb = PreFixed.withIncomeTax(123L, information, percentage(13.0));
 
-        assertEquals(123L, treasury.id());
-        assertEquals(String.format("Tesouro Préfixado %d %s", MATURITY.getYear(), treasury.interest()), treasury.name());
-        assertEquals(INFORMATION.price, treasury.price());
-        assertEquals(Issuer.government(), treasury.issuer());
-        assertEquals(INFORMATION.maturity, treasury.maturity());
-        assertEquals(0.13, treasury.interest().rate());
-        assertEquals(0.225, treasury.incomeTaxRate());
-        assertTrue(treasury.isActive());
+        assertEquals(123L, cdb.id());
+        assertEquals(String.format("CDB %d %s", information.maturity.getYear(), cdb.interest()), cdb.name());
+        assertEquals(information.price, cdb.price());
+        assertEquals(information.issuer, cdb.issuer());
+        assertEquals(information.maturity, cdb.maturity());
+        assertEquals(0.13, cdb.interest().rate());
+        assertEquals(0.225, cdb.incomeTaxRate());
+        assertTrue(cdb.isActive());
+        assertFalse(cdb.haveIncomeTaxException());
     }
 
     @Test
-    public void createCdb() {
-        var cdb = PreFixed.cdb(123L,  someInformation().maturity(MATURITY), percentage(10.0));
+    public void createWithoutIncomeTax() {
+        var information = someLCAInformationWith(MATURITY);
+        var lca = PreFixed.withoutIncomeTax(123L, information, percentage(8.0));
 
-        assertEquals(123L, cdb.id());
-        assertEquals(String.format("CDB %s %d %s", INFORMATION.issuer, MATURITY.getYear(), cdb.interest()), cdb.name());
-        assertEquals(INFORMATION.price, cdb.price());
-        assertEquals(INFORMATION.issuer, cdb.issuer());
-        assertEquals(INFORMATION.maturity, cdb.maturity());
-        assertEquals(0.1, cdb.interest().rate());
-        assertEquals(0.225, cdb.incomeTaxRate());
-        assertTrue(cdb.isActive());
+        assertEquals(123L, lca.id());
+        assertEquals(String.format("LCA %d %s", information.maturity.getYear(), lca.interest()), lca.name());
+        assertEquals(information.price, lca.price());
+        assertEquals(information.issuer, lca.issuer());
+        assertEquals(information.maturity, lca.maturity());
+        assertEquals(0.08, lca.interest().rate());
+        assertEquals(0.0, lca.incomeTaxRate());
+        assertTrue(lca.isActive());
+        assertTrue(lca.haveIncomeTaxException());
     }
 
     @Test
     public void incomeTaxRate() {
-        var incomeTaxRates = List.of(
-                PreFixed.lca(123L, someInformation(), percentage(5))
-                        .incomeTaxRate(),
-                PreFixed.cdb(123L, someInformation().maturity(NOW.plusDays(721)), percentage(10.0))
-                        .incomeTaxRate(),
-                PreFixed.cdb(123L, someInformation().maturity(NOW.plusDays(361)), percentage(5.0))
-                        .incomeTaxRate(),
-                PreFixed.cdb(123L, someInformation().maturity(NOW.plusDays(181)), percentage(5.0))
-                        .incomeTaxRate(),
-                PreFixed.cdb(123L, someInformation().maturity(NOW), percentage(5.0)).incomeTaxRate());
+        var incomeTaxRates = Stream.of(
+                PreFixed.withoutIncomeTax(1L, someLCAInformationWith(NOW), percentage(5)),
+                PreFixed.withIncomeTax(2L, someCDBInformationWith(NOW.plusDays(721)), percentage(5)),
+                PreFixed.withIncomeTax(3L, someCDBInformationWith(NOW.plusDays(361)), percentage(5)),
+                PreFixed.withIncomeTax(4L, someCDBInformationWith(NOW.plusDays(181)), percentage(5)),
+                PreFixed.withIncomeTax(5L, someCDBInformationWith(NOW), percentage(5.0)))
+                .map(Bond::incomeTaxRate).collect(Collectors.toList());
 
         assertEquals(List.of(0.0, 0.15, 0.175, 0.20, 0.225), incomeTaxRates);
     }
 
     @Test
-    public void createLca() {
-        var lca = PreFixed.lca(123L,  someInformation().maturity(MATURITY), percentage(8.0));
-
-        assertEquals(123L, lca.id());
-        assertEquals(String.format("LCA %s %d %s", INFORMATION.issuer, MATURITY.getYear(), lca.interest()), lca.name());
-        assertEquals(INFORMATION.price, lca.price());
-        assertEquals(INFORMATION.issuer, lca.issuer());
-        assertEquals(INFORMATION.maturity, lca.maturity());
-        assertEquals(0.08, lca.interest().rate());
-        assertEquals(0.0, lca.incomeTaxRate());
-        assertTrue(lca.isActive());
-    }
-
-    @Test
-    public void createLci() {
-        var lca = PreFixed.lci(123L,  someInformation().maturity(MATURITY), percentage(8.0));
-
-        assertEquals(123L, lca.id());
-        assertEquals(String.format("LCI %s %d %s", INFORMATION.issuer, MATURITY.getYear(), lca.interest()), lca.name());
-        assertEquals(INFORMATION.price, lca.price());
-        assertEquals(INFORMATION.issuer, lca.issuer());
-        assertEquals(INFORMATION.maturity, lca.maturity());
-        assertEquals(0.08, lca.interest().rate());
-        assertEquals(0.0, lca.incomeTaxRate());
-        assertTrue(lca.isActive());
-    }
-
-    @Test
-    public void createCra() {
-        var lca = PreFixed.cra(123L, someInformation().maturity(MATURITY), percentage(8.0));
-
-        assertEquals(123L, lca.id());
-        assertEquals(String.format("CRA %s %d %s", INFORMATION.issuer, MATURITY.getYear(), lca.interest()), lca.name());
-        assertEquals(INFORMATION.price, lca.price());
-        assertEquals(INFORMATION.issuer, lca.issuer());
-        assertEquals(INFORMATION.maturity, lca.maturity());
-        assertEquals(0.08, lca.interest().rate());
-        assertEquals(0.0, lca.incomeTaxRate());
-        assertTrue(lca.isActive());
-    }
-
-    @Test
-    public void createCri() {
-        var lca = PreFixed.cri(123L, someInformation().maturity(MATURITY), percentage(8.0));
-
-        assertEquals(123L, lca.id());
-        assertEquals(String.format("CRI %s %d %s", INFORMATION.issuer, MATURITY.getYear(), lca.interest()), lca.name());
-        assertEquals(INFORMATION.price, lca.price());
-        assertEquals(INFORMATION.issuer, lca.issuer());
-        assertEquals(INFORMATION.maturity, lca.maturity());
-        assertEquals(0.08, lca.interest().rate());
-        assertEquals(0.0, lca.incomeTaxRate());
-        assertTrue(lca.isActive());
-    }
-
-    @Test
-    public void throwsInvalidEntityExceptionWhenPassInvalidInformation() {
-        var exception = assertThrows(Entity.InvalidEntity.class, () ->
-                PreFixed.treasury(123L, someInformation().price(-1.0), percentage(5)));
-
-        assertEquals("pre-fixed bond", exception.errors().context());
-        assertThat(exception.errors().reasons())
-                .hasSize(1)
-                .containsAll(List.of("price must be greater 0.0"));
-    }
-
-    @Test
     public void throwsInvalidEntityExceptionWhenPassInvalidValueObjects() {
         var exception = assertThrows(Entity.InvalidEntity.class, () ->
-                PreFixed.cdb(123L, someInvalidInformation(), percentage(-5)));
+                PreFixed.withoutIncomeTax(123L, someInformationWithAllFieldsInvalid(), percentage(-5)));
 
         assertEquals("pre-fixed bond", exception.errors().context());
         assertThat(exception.errors().reasons())
-                .hasSize(3)
+                .hasSize(5)
                 .containsAll(List.of(
+                        "bond name must be in ['LCA', 'LCI', 'CRA', 'CRI']",
+                        "bond name must contain 3 to 17 letters",
                         "price must be greater 0.0",
                         "issuer name must contain 5 to 10 letters",
                         "interest rate must be greater 0.0"));
@@ -146,5 +82,4 @@ public class PreFixedTest {
 
     private static final LocalDate NOW = LocalDate.now();
     private static final LocalDate MATURITY = NOW.plusDays(10);
-    private static final Information INFORMATION = someInformation().maturity(MATURITY).build();
 }
